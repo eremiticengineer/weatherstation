6e60478cbddb2c91d72fe5dd4ad39e3d1c1c544a
eremiticengineer <dev@eremiticengineer.com>
2025-06-28 15:20:10 +0100

    Update configure

--- src/VEML7700/VEML7700.cpp
+++ src/VEML7700/VEML7700.cpp
@@ -10,11 +10,11 @@ VEML7700::VEML7700(i2c_inst_t *i2c_inst, uint8_t address, uint sda, uint scl)
     : i2c(i2c_inst), i2c_address(address), sda_pin(sda), scl_pin(scl) {}
 
 bool VEML7700::begin() {
-    i2c_init(i2c, 100 * 1000); // 100 kHz
-    gpio_set_function(sda_pin, GPIO_FUNC_I2C);
-    gpio_set_function(scl_pin, GPIO_FUNC_I2C);
-    gpio_pull_up(sda_pin);
-    gpio_pull_up(scl_pin);
+    // i2c_init(i2c, 100 * 1000); // 100 kHz
+    // gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+    // gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+    // gpio_pull_up(sda_pin);
+    // gpio_pull_up(scl_pin);
 
     configure();
 
@@ -23,12 +23,29 @@ bool VEML7700::begin() {
     return readRegister(VEML7700_ALS_CONF, dummy);
 }
 
+// void VEML7700::configure() {
+//     // Set ALS gain = 1x, integration time = 100 ms, ALS enable
+//     uint16_t config = 0x0000;
+//     config |= (0x00 << 11); // Gain 1x
+//     config |= (0x02 << 6);  // IT 100 ms
+//     config |= (0x00 << 1);  // ALS power on
+
+//     writeRegister(VEML7700_ALS_CONF, config);
+// }
+
 void VEML7700::configure() {
-    // Set ALS gain = 1x, integration time = 100 ms, ALS enable
+    // Gain = 1x      (00 << 11)
+    // IT = 100ms     (110 << 6)
+    // Persistence = 1 (00 << 4)
+    // Power on       (0 << 1)
+    // ALS enable     (0 << 0)
+
     uint16_t config = 0x0000;
-    config |= (0x00 << 11); // Gain 1x
-    config |= (0x02 << 6);  // IT 100 ms
-    config |= (0x00 << 1);  // ALS power on
+    config |= (0x0 << 11); // gain: 00 = 1x
+    config |= (0x6 << 6);  // IT: 110 = 100ms
+    config |= (0x0 << 4);  // persistence: 00 = 1 sample
+    config |= (0x0 << 1);  // power mode: 0 = on
+    config |= (0x0 << 0);  // ALS enable: 0 = ALS on
 
     writeRegister(VEML7700_ALS_CONF, config);
 }
